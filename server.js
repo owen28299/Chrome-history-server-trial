bd4952690a16b7ad6546b41d0226567f177c4e7e
Owen <owenyangg@gmail.com>
2016-05-14 16:48:09 -1000

    allows iteration through urls via get request

--- server.js
+++ server.js
@@ -6,11 +6,37 @@ const express        = require('express'),
       PORT           = process.env.PORT || 3000
       ;
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true }));
+app.use(bodyParser.json({limit: '50mb'}));
 
-app.use(express.static(__dirname + '/public'));
+var urls = {};
 
+app.post('/data', function(req,res){
+  urls = JSON.parse(req.body.urls);
+  res.send("sucess");
+});
+
+app.get('/data/:terms', function(req,res){
+  var terms = req.params.terms;
+
+  terms = terms.split(' ');
+
+  var candidates = [];
+
+  for (var i in urls){
+    for (var j in terms){
+
+      if (i.indexOf(terms[j]) !== -1){
+        candidates.push(i);
+      }
+    }
+  }
+
+  console.log(candidates);
+
+  res.send("hello");
+
+});
 
 app.listen(PORT, function(){
   console.log(`Server listening on port: ${PORT}`);
